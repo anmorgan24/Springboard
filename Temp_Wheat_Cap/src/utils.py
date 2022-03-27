e531041079386a67e28c2a2e655207ca5fd0a3b0
Abigail Morgan <anmorgan24@gmail.com>
2022-03-27 13:06:13 -0400

    removed redundant code

--- Temp_Wheat_Cap/src/utils.py
+++ Temp_Wheat_Cap/src/utils.py
@@ -9,6 +9,10 @@ import matplotlib.pyplot  as plt
 from matplotlib import rcParams
 from sklearn.utils import shuffle
 
+
+
+
+
 def set_plot(size):
     
     """Sets style preferences and text sizes for matplotlib plots."""
@@ -25,8 +29,8 @@ def set_plot(size):
     plt.rc('axes', labelsize=size)    # fontsize of the x and y labels
     plt.rc('xtick', labelsize=size*0.8)    # fontsize of the tick labels
     plt.rc('ytick', labelsize=size*0.8)    # fontsize of the tick labels
-
-
+    
+    
 def balance_df(df, label='label', class_size=1000):
     
     """Resamples data frame containing class labels so that every class has an equal class size. 
@@ -45,7 +49,7 @@ def balance_df(df, label='label', class_size=1000):
     
     for i in range(n_classes):
         one_class = df[df[label] == i]
-        if len(one_class) >= class_size:
+        if len(one_class) >= 2000:
             replace=False
         else:
             replace=True
@@ -57,3 +61,36 @@ def balance_df(df, label='label', class_size=1000):
     balanced_df = balanced_df.sample(frac=1).reset_index().rename(columns={'index':'old_index'})
 
     return balanced_df
+
+
+# Transform and preprocess imported df
+
+def preprocess_df(df):
+    bbox_xmin = []
+    bbox_ymin = []
+    bbox_width = []
+    bbox_height = []
+    for i, row in df.iterrows():
+        row['image_name'] = (row.image_name.split('.')[0]+'.jpg')
+    for i, row in df.iterrows():
+        row['BoxesString'] = list(row['BoxesString'].split(';'))
+    df = df.explode('BoxesString')
+    for i, row in df.iterrows():
+        row['BoxesString'] = list(row['BoxesString'].split(' '))
+    df.BoxesString = df.BoxesString.apply(lambda y: list('0 0 0 0'.split(" ")) if len(y)==1 else y)
+    df_lst = list(df.BoxesString)
+    for bbox_lst in df_lst:
+        bbox_xmin.append(bbox_lst[0])
+        bbox_ymin.append(bbox_lst[1])
+        bbox_width.append(bbox_lst[2])
+        bbox_height.append(bbox_lst[3])
+    df['bbox_xmin'] = bbox_xmin
+    df['bbox_ymin'] = bbox_ymin
+    df['bbox_xmax'] = bbox_width
+    df['bbox_ymax'] = bbox_height
+    df.reset_index(inplace=True, drop=True)
+    df = df.astype({'bbox_xmin': 'float', 'bbox_ymin': 'float', 'bbox_xmax': 'float', 'bbox_ymax': 'float'})
+    return df
+    
+
+
